996ec12b364be2a8afb672681c723780cd8c84f0
rokan45 <rokanuzzaman4567@gmail.com>
2026-03-27 03:06:28 +0600

    an react app initialization that in purpose of cricket auction

---
+++ src/App.jsx
@@ -0,0 +1,15 @@
+import { Menu } from 'lucide-react'
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <>
+    <h1 className='text-6xl'>React App</h1>
+    <button className='btn'>click here</button>
+    </>
+  )
+}
+
+export default App
